a81e3175f52ca708520fdd9dc81fb9661c6c8ade
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2025-01-09 14:37:54 +0900

    Update solution.py

--- INU_Code_Festival_2024/횃불이 키우기/Solution/solution.py
+++ INU_Code_Festival_2024/횃불이 키우기/Solution/solution.py
@@ -2,35 +2,26 @@ import sys
 
 input = sys.stdin.readline
 
-def main():
-    mascot_size = solution()
-    if mascot_size > 10 ** 11:
-        print("MEGA")
-    elif mascot_size <= 0:
-        print("-1")
-    else:
-        print(mascot_size)
-
-def solution():
-    n,k,s = map(int,input().split())
-    grow = list(map(int,input().split()))
+n,k,s = map(int,input().split())
+grow = list(map(int,input().split()))
     
-    if k > 36:
-        return 10 ** 11 + 1
+if k > 36: print("MEGA"); exit(0)
 
-    dp = [[0]*(k+1) for _ in range(n+1)]
+dp = [[0]*(k+1) for _ in range(n+1)]
+dp[0][0] = s
     
-    dp[0][0] = s
-    
-    for i in range(n):
-        for j in range(k):
-            if dp[i][j] <= 0:
-                continue
-            dp[i+1][j] = max(dp[i+1][j], dp[i][j] + grow[i])
-            dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] * 2)
-        if dp[i][k] > 0:
-            dp[i+1][k] = max(dp[i+1][k],dp[i][k]+grow[i])
-    return max(dp[n]) #n일차 횃불이가 가장 클 때의 사이즈
+for i in range(n):
+    for j in range(k):
+        if dp[i][j] <= 0: continue
+        dp[i+1][j] = max(dp[i+1][j], dp[i][j] + grow[i])
+        dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] * 2)
+    if dp[i][k] > 0: dp[i+1][k] = max(dp[i+1][k],dp[i][k]+grow[i])
 
-if __name__ == "__main__":
-    main()
+mascot_size = max(dp[n])
+
+if mascot_size > 10 ** 11:
+    print("MEGA")
+elif mascot_size <= 0:
+    print("-1")
+else:
+    print(mascot_size)
